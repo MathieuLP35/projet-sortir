043f594e805dbe603f46f24b653815c8dc284570
Nolwenn <UNiK@UNIK-PSE06UQOK4.home>
2024-02-20 11:53:43 +0100

    suppression formulaire et make:crud

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,43 +3,79 @@
 namespace App\Controller;
 
 use App\Entity\Event;
-use App\Form\RegisterEventFormType;
+use App\Form\EventType;
 use App\Repository\EventRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/event')]
 class EventController extends AbstractController
 {
-    #[Route('/event', name: 'app_event')]
+    #[Route('/', name: 'app_event_index', methods: ['GET'])]
     public function index(EventRepository $eventRepository): Response
     {
         return $this->render('event/index.html.twig', [
-            'events' => $eventRepository,
+            'events' => $eventRepository->findAll(),
         ]);
     }
-    #[Route('/register-for-event', name: 'register_for_event')]
-    public function registerForEvent(Request $request, EntityManagerInterface $entityManager): Response
+
+    #[Route('/new', name: 'app_event_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $event = new Event();
-        $form = $this->createForm(RegisterEventFormType::class, $event);
-
+        $form = $this->createForm(EventType::class, $event);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            // Traitement de l'inscription à l'événement
-            // Enregistrement en base de données, etc.
-
             $entityManager->persist($event);
             $entityManager->flush();
 
-            // Rediriger ou faire d'autres actions après l'inscription
+            return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->render('event/register_for_event.html.twig', [
-            'form' => $form->createView(),
+        return $this->render('event/new.html.twig', [
+            'event' => $event,
+            'form' => $form,
         ]);
+    }
+
+    #[Route('/{id}', name: 'app_event_show', methods: ['GET'])]
+    public function show(Event $event): Response
+    {
+        return $this->render('event/show.html.twig', [
+            'event' => $event,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_event_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Event $event, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('event/edit.html.twig', [
+            'event' => $event,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_event_delete', methods: ['POST'])]
+    public function delete(Request $request, Event $event, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($event);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
     }
 }
