832d03a93d1b11b2a6a1f4673e9a37e87de6a843
Nolwenn <UNiK@UNIK-PSE06UQOK4.home>
2024-02-20 16:46:24 +0100

    modif page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(Security $security): Response
     {
-        return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
-        ]);
+        // Vérifiez si l'utilisateur est connecté
+        if ($security->getUser()) {
+            // Utilisateur connecté, redirigez-le vers la page '/event'
+            return $this->redirectToRoute('app_event_index');
+        }
+
+        // Utilisateur non connecté, redirigez-le vers la page '/login'
+        return $this->redirectToRoute('app_login');
     }
 }
