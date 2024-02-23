be6f8e57186f3a39452ce0989557cc068fa3f38f
Bazmati <basilemalin@gmx.com>
2024-02-23 09:32:20 +0100

    a ne pas merge DANGER

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,11 +12,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(Security $security): Response
     {
-        // // Vérifiez si l'utilisateur est connecté
-        // if ($security->getUser()) {
-        //     // Utilisateur connecté, redirigez-le vers la page '/event'
-        //     return $this->redirectToRoute('app_event_index');
-        // }
+        // Vérifiez si l'utilisateur est connecté
+        if ($security->getUser()) {
+            // Utilisateur connecté, redirigez-le vers la page '/event'
+            return $this->redirectToRoute('app_event_index');
+        }
 
         // Utilisateur non connecté, redirigez-le vers la page '/login'
         return $this->redirectToRoute('app_event_index');
